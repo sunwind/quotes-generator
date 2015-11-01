99c1cc24f16686eba10a69ef6abee8d9aa6d6212
Weike Dong <kyle@sincway.com>
2015-11-01 22:24:39 +0800

    Changed tab indent to four whitespaces to match the PEP 8 standard

--- get_quote.py
+++ get_quote.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import cgi
 from random import randint
@@ -12,22 +12,23 @@ version = arguments.getvalue('v', 'txt')
 
 # 从一个  tuple 或者 list 中随机获取一项
 def get_random_item(some_list):
-	return some_list[randint(0, len(some_list)-1)]
+    return some_list[randint(0, len(some_list)-1)]
 
 # 从外部文件读取姓名列表。姓名列表来自 https://github.com/dominictarr/random-name
 def get_name_list(filename):
-	name_list = []
-	with open(filename, 'r') as name_file:
-		for line in name_file.readlines():
-			name_list.append(line.strip())
+    name_list = []
+    with open(filename, 'r') as name_file:
+        for line in name_file.readlines():
+            name_list.append(line.strip())
 
-	return name_list
+    return name_list
 
 # 姓名列表
 first_names = get_name_list('first_name.txt')
 last_names = get_name_list('last_name.txt')
 
 # 各个可选的列表
+# FIXME： 这些应该在配置文件里
 moods = ('快乐', '悲伤', '兴奋', '愉悦', '失落', '寂寥', '焦躁', '孤独', '孤单', '无力', '空虚', '相见恨晚')
 countries = ('英国', '法国', '美国', '爱尔兰', '澳大利亚', '加拿大', '德国', '芬兰', '挪威', '新西兰', '瑞士', '意大利', '瑞典')
 professions = ('小说家', '作家', '设计师', '画家', '社会学家', '学者', '艺术家')
@@ -39,30 +40,31 @@ company_types = ('LLC', 'GmbH', 'Ltd.', 'AG', 'Corp.', 'Inc.')
 job_titles = ('CEO', '系统架构师', '资深开发工程师', '项目经理', '技术总监', 'CTO', '总裁')
 descriptions = ('爱不释手', '感到随心所欲', '如虎添翼', '始终保持高效', '披荆斩棘，无所不能', '心无旁骛', '动力十足', '非常满意')
 
+# 随机决定使用哪个模板
 random_int = randint(0, 3)
 
 if random_int == 0:
-	# 生成用于格式化字符串的参数
-	args_list = (None, None)
+    # 生成用于格式化字符串的参数
+    args_list = (None, None)
 
-	while args_list[0] == args_list[1] :	# 避免重复生成两个相同的 mood
-		# map 的结果是 list (Python 3 下是 map object)，必须转为 tuple 才可以用于格式化字符串
-		args_list = tuple(map(get_random_item, (moods, moods, countries, professions, first_names, last_names)))
+    while args_list[0] == args_list[1] :    # 避免重复生成两个相同的 mood
+        # map 的结果是 list (Python 3 下是 map object)，必须转为 tuple 才可以用于格式化字符串
+        args_list = tuple(map(get_random_item, (moods, moods, countries, professions, first_names, last_names)))
 
-	# 生成第一种「名言」
-	quote = '%s的本质是一种隐藏的%s。  --- %s著名%s %s %s'  % args_list		
+    # 生成第一种「名言」
+    quote = '%s的本质是一种隐藏的%s。  --- %s著名%s %s %s'  % args_list        
 
 elif random_int == 1:
-	args_list = tuple(map(get_random_item, (languages, advantages, descriptions, companies, company_types, job_titles, first_names, last_names)))
-	quote = '我认为 %s 是世界上最好的语言，它的%s让我们的团队%s。 --- %s %s %s %s %s' % args_list
+    args_list = tuple(map(get_random_item, (languages, advantages, descriptions, companies, company_types, job_titles, first_names, last_names)))
+    quote = '我认为 %s 是世界上最好的语言，它的%s让我们的团队%s。 --- %s %s %s %s %s' % args_list
 
 elif random_int == 2:
-	args_list = tuple(map(get_random_item, (fruits, moods, countries, professions, first_names, last_names)))
-	quote = '%s是甜的。这是一种让人感到%s的甜。 --- %s著名%s %s %s' % args_list
+    args_list = tuple(map(get_random_item, (fruits, moods, countries, professions, first_names, last_names)))
+    quote = '%s是甜的。这是一种让人感到%s的甜。 --- %s著名%s %s %s' % args_list
 
 elif random_int == 3:
-	args_list = tuple(map(get_random_item, (languages, fruits, advantages, companies, job_titles, first_names, last_names)))
-	quote = '%s 语言就像%s一样，需要细细品味，才能充分体会它那愈久弥深的%s。 --- %s 公司 %s : %s %s' % args_list
+    args_list = tuple(map(get_random_item, (languages, fruits, advantages, companies, job_titles, first_names, last_names)))
+    quote = '%s 语言就像%s一样，需要细细品味，才能充分体会它那愈久弥深的%s。 --- %s 公司 %s : %s %s' % args_list
 
 if version == 'txt':
     print('Content-Type: text/plain')
