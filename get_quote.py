890746fc5d58135994ed317b7399d46fd879d300
Weike Dong <kyle@sincway.com>
2015-11-05 20:23:56 +0800

    Added template support. Now the code does not contain any specific template strings or arguments, as they are now automatically discovered inside their own folders one by one

--- get_quote.py
+++ get_quote.py
@@ -3,6 +3,7 @@
 
 import cgi
 import sys
+import os
 from random import randint
 
 # 启用 CGI 调试信息显示
@@ -18,7 +19,7 @@ version = arguments.getvalue('v', 'txt')
 def get_random_item(some_list):
     return some_list[randint(0, len(some_list)-1)]
 
-# 从外部文件读取到列表。姓名列表来自 https://github.com/dominictarr/random-name
+# 从外部列表文件读取到列表
 def get_list_from_file(filepath):
     the_list = []
     with open(filepath, 'r') as name_file:
@@ -28,67 +29,65 @@ def get_list_from_file(filepath):
 
     return the_list
 
-# 从列表名得到文件地址
-def get_path(list_name):
-    return 'lists/' + list_name + '.txt'
-
-# 各种列表的名字
-list_names = ['first_names', 'last_names', 'moods', 'countries', 'professions', 'fruits', 'languages', 'advantages',
-              'companies', 'company_types', 'job_titles', 'descriptions']
-
-# 得到以上各个列表名字存放的地址
-file_path_list = list(map(get_path, list_names))
 
 # 存放各个参数的词典，比如 args_dict['moods'] 等
 args_dict = {}
 
 # 读取列表到词典
 try:
-    args_result = list(map(get_list_from_file, file_path_list))
+    for dirpath, dirnames, filenames in os.walk('lists'):
+        for some_list_file in filenames:
+            with open(os.path.join('lists', some_list_file), 'r') as some_list:
+                all_content = some_list.read().split()
+
+                all_content = [x for x in all_content if len(x.strip())]
+
+            args_dict[some_list_file.split('.txt')[0]] = all_content
 except Exception as e:
     print('Content-Type: text/plain')
     print('')
     print(e)
     sys.exit(1)
 
-for item in enumerate(list_names):  # enumerate 挺好用的
-    args_dict[item[1]] = args_result[item[0]]
 
-# TODO: 模板要分离出去
-# FIXME: 减少逻辑和模板的耦合度
+# 用来存放模板和参数字典的列表
+templates = []
+
+# TODO 提高效率。for 太多了。做一些 profiling
+
+# 遍历模板文件夹的每一个模板
+for dirpath, dirnames, filenames in os.walk('templates'):
+    for template_file in filenames:
+        with open(os.path.join('templates', template_file), 'r') as one_template_file:
+            template_set = {}
+
+            for line in one_template_file:
+                if len(line.strip()) and line[0] != '#':   # 如果不是注释行
+                    if 'template' not in template_set:
+                        template_set['template'] = line
+                    elif 'types' not in template_set:
+                        template_set['types'] = line
+                    else:
+                        break
+
+            templates.append(template_set)
 
 # 随机决定使用哪个模板
-random_int = randint(0, 3)
+random_int = randint(0, len(templates)-1)
 
-if random_int == 0:
-    # 生成用于格式化字符串的参数
-    args_list = (None, None)
-
-    while args_list[0] == args_list[1] :    # 避免重复生成两个相同的 moods
-        # 模板中对应的类型 keys
-        args_keys_list = ['moods', 'moods', 'countries', 'professions', 'first_names', 'last_names']
-
-        # 从 args_list 词典中随机提取 key 对应的列表的一项作为参数
-        args_list = tuple(map(get_random_item, map(args_dict.get, args_keys_list)))
-
-    # 生成第一种「名言」
-    quote = '%s的本质是一种隐藏的%s。  --- %s著名%s %s %s'  % args_list        
-
-elif random_int == 1:
-    args_keys_list = ['languages', 'advantages', 'descriptions', 'companies', 'company_types', 'job_titles',
-                      'first_names', 'last_names']
+# 随机得到 types 指定类别的参数列表
+try:
+    args_keys_list = templates[random_int]['types'].split()
     args_list = tuple(map(get_random_item, map(args_dict.get, args_keys_list)))
-    quote = '我认为 %s 是世界上最好的语言，它的%s让我们的团队%s。 --- %s %s %s %s %s' % args_list
+except TypeError as e:
+    print('Content-Type: text/plain')
+    print('')
+    print('有一只模板文件出了问题，请检查 templates 目录下的模板。')
+    sys.exit(1)
 
-elif random_int == 2:
-    args_keys_list = ['fruits', 'moods', 'countries', 'professions', 'first_names', 'last_names']
-    args_list = tuple(map(get_random_item, map(args_dict.get, args_keys_list)))
-    quote = '%s是甜的。这是一种让人感到%s的甜。 --- %s著名%s %s %s' % args_list
-
-elif random_int == 3:
-    args_keys_list = ['languages', 'fruits', 'advantages', 'companies', 'job_titles', 'first_names', 'last_names']
-    args_list = tuple(map(get_random_item, map(args_dict.get, args_keys_list)))
-    quote = '%s 语言就像%s一样，需要细细品味，才能充分体会它那愈久弥深的%s。 --- %s 公司 %s : %s %s' % args_list
+# 按照模板生成一句名言
+quote = templates[random_int]['template'] % args_list
+quote = quote.strip()
 
 if version == 'txt':
     print('Content-Type: text/plain')
